a3bee31aecf58f6adc85161b1bfbfdf7586c132a
Brandon Chase <brandonjameschase@gmail.com>
2017-10-05 23:27:46 -0500

    Parallel sort on data and save to individual files

--- part1/src/main.cpp
+++ part1/src/main.cpp
@@ -1,35 +1,54 @@
 #include <string>
 #include <deque>
 #include <iostream>
-#include <iomanip> //used for testing outputting numbers
-#include <limits> //used for testing outputting numbers
+#include <iomanip> 
+#include <limits> 
 #include <fstream>
+#include <chrono>
+#include <sys/wait.h>
+
+#include <assert.h>
+#include <unistd.h>
+#include <string.h>
 
 using namespace std;
 
-const string FILENAME = "../earthquakes.csv";
+//filenames assume running from part directory
+const string INPUT_FILENAME = "../earthquakes.csv";
+const string TEMP_FILENAME = "latitudes";
+const string SORT_PROGRAM_PATH = "./bin/sort";
 
-int timeSort(int num_processes, int num_lines);
+long timeSort(deque<double> content, int num_lines, int num_processes);
 deque<double> loadLatitudes(string filename);
 deque<string> splitString(string content, string delimeters);
 void saveDataChunk(string filename, deque<double> content, int start_index, int end_index);
+void splitAndSave(string filename, deque<double> content, int num_of_lines, int num_of_chunks);
+void performParallelSort(int num_processes);
 
 int main(int argc, const char * argv[])
 {
-	deque<double> latitudes = loadLatitudes(FILENAME);
-	for(int i = 0; i < 10; i++)
-	{
-		cout << setprecision (std::numeric_limits<double>::digits10 + 1) << latitudes.at(i) << endl;
-	}
-
-	saveDataChunk("testchunk", latitudes, 0, 99);
-	int temp;
-	cin >> temp;
+	deque<double> data;
+	data.push_back(9);
+	data.push_back(8);
+	data.push_back(7);
+	data.push_back(6);
+	data.push_back(5);
+	data.push_back(4);
+	data.push_back(3);
+	data.push_back(2);
+	data.push_back(1);
+	data.push_back(0);
+	timeSort(data, data.size(), 2);
 	//ask if user wants to do custom parameters for sort and get parameters
 	//if user specifies, do their desired operation
 	//if not, do for 1, 2, 4, and 10 processes
 	//load data as vector of strings
 	//call the time to sort and print out results
+	//delete temp files
+	//save sorted que
+
+	char temp;
+	cin >> temp;
 	return 0;
 }
 
@@ -39,16 +58,22 @@ Parameters: 	num_processes is the number of processes that will handle the sort
 Parameters: 	num_lines is how much of the data is to be sorted, by default it is whole file unless another value is specified by the user
 Returns: 		the time in seconds it takes to do the sort
 */
-int timeSort(int num_processes, int num_lines)
+long timeSort(deque<double> content, int num_lines, int num_processes)
 {
 	//start timer
+	auto start = chrono::high_resolution_clock::now();
+
 	//split data into chunks and save chunks
-	//do forks to split into number of processes
-	//perform the sort on each chunk in each process
+	splitAndSave(TEMP_FILENAME, content, num_lines, num_processes);
+
+	performParallelSort(num_processes);
+
 	//read sorted chunks into lists
 	//merge chunks using list of lists and doing top of deck of cards process
-	//stop timer and figure out how long it took
-	//return time span
+
+	//stop timer and return how long it took
+	auto end = chrono::high_resolution_clock::now();
+    return chrono::duration_cast<chrono::milliseconds>(end-start).count();
 }
 
 // Loads the latitiudes of earthquakes (doubles) from a file into a deque. 
@@ -115,7 +140,7 @@ deque<string> splitString(string content, string delimeters)
 	return result;
 }
 
-// Saves a chunk of data from a list of data specified from a starting point to an ending point in list
+// Saves a chunk of data from a list of data specified from a starting point to an ending point (inclusive) in list
 void saveDataChunk(string filename, deque<double> content, int start_index, int end_index)
 {
 	//go from start index to end index and write data from that span into file, one element per line
@@ -128,3 +153,68 @@ void saveDataChunk(string filename, deque<double> content, int start_index, int 
 	file.close();
 }
 
+// Splits given deque into equal length sections and saves the sections into files
+// ex: saves 3 chunks to filename0, filename1, filename2
+void splitAndSave(string filename, deque<double> content, int num_of_lines, int num_of_chunks)
+{
+	assert(num_of_lines >= 0 && num_of_lines <= content.size());
+	
+	int lines_per_chunk = num_of_lines / num_of_chunks;
+
+	for(int i = 0; i < num_of_chunks; i++)
+	{
+		string chunk_filename = filename + to_string(i);
+		int starting_index = i * lines_per_chunk;
+
+		int ending_index;
+		if(i == num_of_chunks - 1) //saving last chunk
+		{
+			/*
+				If content isn't divided evenly, ie num lines % num chunks != 0, include all of last chunk that would otherwide be cut off.	
+				Set to content.size() - 1 b/c end_index is inclusive in saveChunk function.
+			*/
+			ending_index = num_of_lines - 1; 
+		}
+
+		else
+		{
+			ending_index = starting_index + lines_per_chunk - 1;
+		}
+
+		saveDataChunk(chunk_filename, content, starting_index, ending_index);
+	}
+}
+
+void performParallelSort(int num_processes)
+{
+	//do forks to split into number of processes
+	for(int i = 0; i < num_processes; i++)
+	{
+		pid_t pid = fork();
+		if(pid == 0)
+		{
+			//in child process
+			string filename = TEMP_FILENAME+to_string(i);
+			//perform sort on ith chunk of data
+
+			char* c_path = new char[SORT_PROGRAM_PATH.length() + 1];
+			strcpy(c_path, SORT_PROGRAM_PATH.c_str());
+			char* c_filename = new char[filename.length() + 1];
+			strcpy(c_filename, filename.c_str());
+
+			char *const args[3] = {c_path, c_filename, NULL};
+			execv(args[0], args);
+			exit(0); //end child process once sort is complete
+		}
+
+		else if(pid < 0) //if fork failed
+		{
+			cerr << "Failure to fork()." << endl;
+		}
+
+		//parent process will continue to create more children processes while child processes do sorts
+	}
+
+	//wait for all children to finish sorting
+	while(wait(NULL) > 0);
+}
--- part1/src/sort.cpp
+++ part1/src/sort.cpp
@@ -1,7 +1,105 @@
+#include <string>
+#include <deque>
+#include <iostream>
+#include <iomanip> //used for testing outputting numbers
+#include <limits> //used for testing outputting numbers
+#include <fstream>
+#include <chrono>
+
+#include <assert.h>
+#include <unistd.h>
+
+using namespace std;
+
+deque<double> loadContent(string filename);
+void saveContent(string filename, deque<double> data);
+deque<double> performInsertionSortOn(deque<double> data);
+void insertInto(deque<double>& data, double item);
+
 int main(int argc, const char * argv[])
 {
-	//load data as vector of strings
-	//do insertion sort for new list
-	//save new list
+	string filename;
+	if(argc > 0) //ensure filepath was passed as argument
+	{
+		filename = argv[1]; //to get execv working, for some reason path of executable had to be passed as argument so second argument is filename
+	}
+	deque<double> data = loadContent(filename);
+	deque<double> sorted_data = performInsertionSortOn(data);
+	saveContent(filename, sorted_data);
 	return 0;
+}
+
+deque<double> loadContent(string filename)
+{
+	deque<double> result;
+	//open file to read
+	ifstream file;
+	file.open(filename);
+
+	if(!file) //check for errors opening file
+	{
+		cerr << "Unable to open file " << filename << endl;
+    	exit(1);   // call system to stop
+	}
+
+	else 
+	{
+		double num;
+		while(file >> num)
+		{
+			result.push_back(num);
+		}
+	}
+
+	file.close();
+	return result;
+}
+
+void saveContent(string filename, deque<double> data)
+{
+	ofstream file;
+	file.open(filename);
+	for(int i = 0; i < data.size(); i++)
+	{
+		file << setprecision (std::numeric_limits<double>::digits10 + 1) << data.at(i) << endl;
+	}
+	file.close();
+}
+
+deque<double> performInsertionSortOn(deque<double> data)
+{
+	deque<double> sorted_result;
+	for(double num : data)
+	{
+		insertInto(sorted_result, num);
+	}
+
+	return sorted_result;
+}
+
+void insertInto(deque<double>& data, double item)
+{
+	if(data.size() == 0)
+	{
+		data.push_back(item);
+	}
+
+	else
+	{
+		for(int i = 0; i < data.size(); i++)
+		{
+			//if found place in deque where item is no longer bigger than element, insert in deque before that element to keep deque sorted and exit loop
+			if(item <= data.at(i))
+			{
+				data.insert(data.begin() + i, item);
+				break;
+			}
+
+			//if reached end without inserting, item is bigger than everything and insert at end
+			else if(i == data.size() - 1) 
+			{
+				data.push_back(item);
+			}
+		}
+	}
 }
