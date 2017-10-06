1b43619f307306d2a3965e7618550ff372571561
Brandon Chase <brandonjameschase@gmail.com>
2017-10-06 18:32:57 -0500

    progress, shut up

--- part1/src/main.cpp
+++ part1/src/main.cpp
@@ -13,7 +13,7 @@
 
 using namespace std;
 
-//filenames assume running from part directory
+//filenames assume running from part1 directory
 const string INPUT_FILENAME = "../earthquakes.csv";
 const string TEMP_FILENAME = "latitudes";
 const string SORT_PROGRAM_PATH = "./bin/sort";
@@ -24,21 +24,29 @@ deque<string> splitString(string content, string delimeters);
 void saveDataChunk(string filename, deque<double> content, int start_index, int end_index);
 void splitAndSave(string filename, deque<double> content, int num_of_lines, int num_of_chunks);
 void performParallelSort(int num_processes);
+deque<double> mergeDataChunks(deque<deque<double>> chunks);
+deque<double> loadContent(string filename);
+void saveContent(string filename, deque<double> data);
+long timeSortTest(deque<double> content, int num_lines, int num_processes);
 
 int main(int argc, const char * argv[])
 {
+	//-----TESTING-----
 	deque<double> data;
-	data.push_back(9);
-	data.push_back(8);
-	data.push_back(7);
-	data.push_back(6);
-	data.push_back(5);
-	data.push_back(4);
-	data.push_back(3);
-	data.push_back(2);
-	data.push_back(1);
-	data.push_back(0);
-	timeSort(data, data.size(), 2);
+	int lines, processes;
+	cout << "***IN TEST MODE***" << endl;
+	cout << "How many lines: ";
+	cin >> lines;
+	cout << "How many processes: ";
+	cin >> processes;
+	assert(lines > 0 && processes > 0);
+
+	for(int i = 0; i < lines; i++)
+	{
+		data.push_front(i); //data will be in reverse order
+	}
+
+	cout << "Sort took " << timeSortTest(data, lines, processes) << " ms using " << processes << " processes." << endl;
 	//ask if user wants to do custom parameters for sort and get parameters
 	//if user specifies, do their desired operation
 	//if not, do for 1, 2, 4, and 10 processes
@@ -46,9 +54,6 @@ int main(int argc, const char * argv[])
 	//call the time to sort and print out results
 	//delete temp files
 	//save sorted que
-
-	char temp;
-	cin >> temp;
 	return 0;
 }
 
@@ -61,19 +66,64 @@ Returns: 		the time in seconds it takes to do the sort
 long timeSort(deque<double> content, int num_lines, int num_processes)
 {
 	//start timer
-	auto start = chrono::high_resolution_clock::now();
-
+	//auto start = chrono::high_resolution_clock::now();
 	//split data into chunks and save chunks
 	splitAndSave(TEMP_FILENAME, content, num_lines, num_processes);
-
+	auto start = chrono::high_resolution_clock::now(); //remove
 	performParallelSort(num_processes);
 
 	//read sorted chunks into lists
-	//merge chunks using list of lists and doing top of deck of cards process
+	deque<deque<double>> chunks;
+	for(int i = 0; i < num_processes; i++)
+	{
+		string chunk_filename = TEMP_FILENAME + to_string(i);
+		chunks.push_back(loadContent(chunk_filename));
+	}
 
+	//merge chunks using list of lists and doing top of deck of cards process
+	deque<double> sorted_latitudes = mergeDataChunks(chunks);
+	auto end = chrono::high_resolution_clock::now(); //remove
+	//save sorted list to file?
+	saveContent("sorted.txt", sorted_latitudes);
 	//stop timer and return how long it took
-	auto end = chrono::high_resolution_clock::now();
+	//auto end = chrono::high_resolution_clock::now();
     return chrono::duration_cast<chrono::milliseconds>(end-start).count();
+}
+
+long timeSortTest(deque<double> content, int num_lines, int num_processes)
+{
+	//start timer
+	//auto start = chrono::high_resolution_clock::now();
+	//split data into chunks and save chunks
+	splitAndSave(TEMP_FILENAME, content, num_lines, num_processes);
+	auto start = chrono::high_resolution_clock::now(); //remove
+	performParallelSort(num_processes);
+	auto end = chrono::high_resolution_clock::now(); //remove
+	cout << "parallel sort took " << chrono::duration_cast<chrono::milliseconds>(end-start).count() << " ms\n";
+
+	//read sorted chunks into lists
+	start = chrono::high_resolution_clock::now(); //remove
+	deque<deque<double>> chunks;
+	for(int i = 0; i < num_processes; i++)
+	{
+		string chunk_filename = TEMP_FILENAME + to_string(i);
+		chunks.push_back(loadContent(chunk_filename));
+	}
+	end = chrono::high_resolution_clock::now(); //remove
+cout << "loading sorted lists took " << chrono::duration_cast<chrono::milliseconds>(end-start).count() << " ms\n";
+	//merge chunks using list of lists and doing top of deck of cards process
+start = chrono::high_resolution_clock::now(); //remove
+	deque<double> sorted_latitudes = mergeDataChunks(chunks);
+	end = chrono::high_resolution_clock::now(); //remove
+	cout << "merging sorted lists took " << chrono::duration_cast<chrono::milliseconds>(end-start).count() << " ms\n";
+	//save sorted list to file?
+	start = chrono::high_resolution_clock::now(); //remove
+	saveContent("sorted.txt", sorted_latitudes);
+	end = chrono::high_resolution_clock::now(); //remove
+	cout << "saving sorted lists took " << chrono::duration_cast<chrono::milliseconds>(end-start).count() << " ms\n";
+	//stop timer and return how long it took
+	//auto end = chrono::high_resolution_clock::now();
+    return 1;
 }
 
 // Loads the latitiudes of earthquakes (doubles) from a file into a deque. 
@@ -102,6 +152,33 @@ deque<double> loadLatitudes(string filename)
 
 			//add second column (latitude) into list after converting to double
 			result.push_back(stod(words.at(1)));
+		}
+	}
+
+	file.close();
+	return result;
+}
+
+// Loads the list of doubles in file and returns as deque of doubles. Used in this project for loading sorted chunks
+deque<double> loadContent(string filename)
+{
+	deque<double> result;
+	//open file to read
+	ifstream file;
+	file.open(filename);
+
+	if(!file) //check for errors opening file
+	{
+		cerr << "Unable to open file " << filename << endl;
+    	exit(1);   // call system to stop
+	}
+
+	else 
+	{
+		double num;
+		while(file >> num)
+		{
+			result.push_back(num);
 		}
 	}
 
@@ -185,6 +262,8 @@ void splitAndSave(string filename, deque<double> content, int num_of_lines, int 
 	}
 }
 
+//creates child processes that each read their own file, perform an insertion on the data, and saves sorted data back to same file.
+//then waits for all children to finish 
 void performParallelSort(int num_processes)
 {
 	//do forks to split into number of processes
@@ -217,4 +296,59 @@ void performParallelSort(int num_processes)
 
 	//wait for all children to finish sorting
 	while(wait(NULL) > 0);
+}
+
+//takes a list of chunks and performs a merge sort on them to return one chunk that is sorted
+//uses technique of comparing top card of each sorted deck and taking smallest card out of all of them
+//and adding it to final deck. perform until all decks empty
+deque<double> mergeDataChunks(deque<deque<double>> chunks)
+{
+	assert(chunks.size() > 0);
+	deque<double> sorted_result;
+		
+	bool sort_complete = true;
+	double arbitrary_large = numeric_limits<double>::max();
+	do
+	{
+		//RESET VALUES 
+		double min_value = arbitrary_large;
+		int min_value_index; //chunk number that contains smallest value
+		//all_chunks_empty = true; remove
+		sort_complete = true;
+
+		//Get smallest value of all chunks
+		for(int i = 0; i < chunks.size(); i++)
+		{
+			//deque<double> current_chunk = chunks.at(i); //remove bc might be faster
+			if(!chunks.at(i).empty())
+			{
+				double current_chunk_smallest = chunks.at(i).front(); //chunk sorted ascending order so smallest on top
+				if(current_chunk_smallest <= min_value)
+				{
+					min_value = current_chunk_smallest; 
+					min_value_index = i;
+				}
+			}
+		}
+
+		if(min_value != arbitrary_large) //no new smallest was set
+		{
+			sort_complete = false;
+			sorted_result.push_back(min_value); //add smallest to sorted list
+			chunks.at(min_value_index).pop_front(); //remove smallest from chunks
+		}
+
+		} while (!sort_complete);
+	return sorted_result;//todo
+}
+
+void saveContent(string filename, deque<double> data)
+{
+	ofstream file;
+	file.open(filename);
+	for(int i = 0; i < data.size(); i++)
+	{
+		file << setprecision (numeric_limits<double>::digits10 + 1) << data.at(i) << endl;
+	}
+	file.close();
 }
